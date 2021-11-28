ad7c7805be4f71ddb94ecb1fb422d278aa37b338
Vinicius Lemos <vlemosdeoliveira@gmail.com>
2021-11-28 17:55:35 -0300

    Implementação findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,5 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
@@ -15,11 +13,10 @@ public class Program {
 		
 		System.out.println(department);
 		
-		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.00, department);
-		
 		//Injeção de dependência sem explicitar a implementação
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		Seller seller = sellerDao.findById(3);		
 		System.out.println(seller);
 
 	}
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,12 +1,25 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao{
 
+	private Connection conn;//Dependencia de conexão com o JDBC
+	
+	public SellerDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+	
 	@Override
 	public void insert(Seller obj) {
 		// TODO Auto-generated method stub
@@ -27,8 +40,45 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			//Query a ser executada
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE seller.Id = ?"
+					);
+			//Id que será buscado
+			st.setInt(1, id);
+			//Executa a query
+			rs = st.executeQuery();
+			//Caso a consulta nao retorne nenhum registro, retorna falso
+			//Caso contrário, um novo objeto Seller é instanciado
+			if(rs.next()) {
+				Department dep = new Department();
+				dep.setId(rs.getInt("DepartmentId"));//Recebe o id do departamento
+				dep.setName(rs.getString("DepName"));//Recebe o nome do departamento
+				Seller obj = new Seller();
+				//Aplica os atributos no objeto Seller
+				obj.setId(rs.getInt("Id"));
+				obj.setName(rs.getString("Name"));
+				obj.setEmail(rs.getString("Email"));
+				obj.setBaseSalary(rs.getDouble("BaseSalary"));
+				obj.setBirthDate(rs.getDate("BirthDate"));
+				obj.setDepartment(dep);//Recebe o objeto Department montado no início do bloco
+				return obj;
+			}
+			return null;
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 	@Override
