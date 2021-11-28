d24ca796c8410520e1c9394d0ec59e82945f8fdd
Vinicius Lemos <vlemosdeoliveira@gmail.com>
2021-11-28 17:26:01 -0300

    SellerDaoJDBC e DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,6 +16,9 @@ public class Program {
 		System.out.println(department);
 		
 		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.00, department);
+		
+		//Injeção de dependência sem explicitar a implementação
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 
