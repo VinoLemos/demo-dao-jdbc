4804734ca0a020626dcbb9159f0f821886155118
Vinicius Lemos <vlemosdeoliveira@gmail.com>
2021-11-28 18:00:19 -0300

    Implementação findById

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -12,30 +12,30 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-public class SellerDaoJDBC implements SellerDao{
+public class SellerDaoJDBC implements SellerDao {
 
-	private Connection conn;//Dependencia de conexão com o JDBC
-	
+	private Connection conn;// Dependencia de conexão com o JDBC
+
 	public SellerDaoJDBC(Connection conn) {
 		this.conn = conn;
 	}
-	
+
 	@Override
 	public void insert(Seller obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void update(Seller obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteById(Integer id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -43,39 +43,41 @@ public class SellerDaoJDBC implements SellerDao{
 		PreparedStatement st = null;
 		ResultSet rs = null;
 		try {
-			//Query a ser executada
+			// Query a ser executada
 			st = conn.prepareStatement(
-					"SELECT seller.*,department.Name as DepName "
-					+ "FROM seller INNER JOIN department "
-					+ "ON seller.DepartmentId = department.Id "
-					+ "WHERE seller.Id = ?"
-					);
-			//Id que será buscado
+					"SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id " + "WHERE seller.Id = ?");
+			// Id que será buscado
 			st.setInt(1, id);
-			//Executa a query
+			// Executa a query
 			rs = st.executeQuery();
-			//Caso a consulta nao retorne nenhum registro, retorna falso
-			//Caso contrário, um novo objeto Seller é instanciado
-			if(rs.next()) {
+			// Caso a consulta nao retorne nenhum registro, retorna falso
+			// Caso contrário, um novo objeto Seller é instanciado recebendo um Department
+			// como parâmetro
+
+			if (rs.next()) {
+
+				// Objeto Department temporário
+
 				Department dep = new Department();
-				dep.setId(rs.getInt("DepartmentId"));//Recebe o id do departamento
-				dep.setName(rs.getString("DepName"));//Recebe o nome do departamento
+				dep.setId(rs.getInt("DepartmentId"));// Recebe o id do departamento
+				dep.setName(rs.getString("DepName"));// Recebe o nome do departamento
+
 				Seller obj = new Seller();
-				//Aplica os atributos no objeto Seller
+
+				// Aplica os atributos no objeto Seller
 				obj.setId(rs.getInt("Id"));
 				obj.setName(rs.getString("Name"));
 				obj.setEmail(rs.getString("Email"));
 				obj.setBaseSalary(rs.getDouble("BaseSalary"));
 				obj.setBirthDate(rs.getDate("BirthDate"));
-				obj.setDepartment(dep);//Recebe o objeto Department montado no início do bloco
+				obj.setDepartment(dep);// Recebe o objeto Department montado no início do bloco
 				return obj;
 			}
 			return null;
-		}
-		catch(SQLException e) {
+		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
-		}
-		finally {
+		} finally {
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
 		}
