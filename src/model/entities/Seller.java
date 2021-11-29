c57c985b4e5dcebda7653a69d242571777dd7589
Vinicius Lemos <vlemosdeoliveira@gmail.com>
2021-11-29 16:18:10 -0300

    Implementação insert

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -7,18 +7,18 @@ import java.util.Objects;
 public class Seller implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
-	private int id;
+	private Integer id;
 	private String name, email;
 	private Date birthDate;
 	private Double baseSalary;
 	
 	private Department department;
-
+	
 	public Seller() {
 		
 	}
-	
-	public Seller(int id, String name, String email, Date birthDate, Double baseSalary, Department department) {
+
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
